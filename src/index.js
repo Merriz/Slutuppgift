9320eea26b243f3fbaf0ed45e22f0f099137fe14
Merriz <77438758+Merriz@users.noreply.github.com>
2022-05-05 23:21:41 +0300

    Requesting & Sorting messages working

--- src/index.js
+++ src/index.js
@@ -1,61 +1,53 @@
 const baseUrl = 'https://ha-slutuppgift-chat-do.westling.workers.dev';
 
 addEventListener('pageshow', init);
-
+//setInterval(init, 10000);
 async function init(){
 
     const request = await fetch(`${baseUrl}/api/messages`, {
-        method: 'POST',
+        method: 'GET',
         headers: {
             'Authorization': 'Bearer N31fRWVMZCtwU0JeZnBQdVBjTmlOImRzcTAxfl08cz1xR2lyWGFJfmo5JC5RNSc=',
             'Content-type': ''
         }
-    });
-    const response = await request;
-    console.log(response);
-
-    // let messageContainer = document.getElementById("divFriendMessages");
-    // for (let i = 0; i < response.length; i++){
-    //     let div = document.createElement("div");
-    //     div.innerHTML = 'User: ' + response[i].user + '' + response[i].message;
-    //     messageContainer.appendChild(div);
-    // }
-
-}
-
-function appendMessages(){
-    const form = document.querySelector('form[name="append"]');
-
-    form.addEventListener('submit', async (event) => {
-        event.preventDefault();
-
-        const form = event.target;
-        const formData = new FormData(form);
-        const formInput = Object.fromEntries(formData);
-        const url = new URL(form.action);
-        const request = await fetch(`${baseUrl}/${url.pathname}`, {
-            method: form.method,
-            headers: {
-                'Authorization': 'Bearer N31fRWVMZCtwU0JeZnBQdVBjTmlOImRzcTAxfl08cz1xR2lyWGFJfmo5JC5RNSc=',
-                'Content-type': ''
-            },
-            body: JSON.stringify(formInput),
-        });
-        const response = await request.json();
-
-        console.log(response);
-
-        // let messageContainer = document.getElementById("divFriendMessages");
-        // for (let i = 0; i < response.length; i++){
-        //     let div = document.createElement("div");
-        //     div.innerHTML = 'User: ' + response[i].user + '' + response[i].message;
-        //     messageContainer.appendChild(div);
-        // }
-
-        //displayMessages(response);
-        //return response.body;
-
-
+    }).then((response) => response.json()).then((data) => {
+        return data
     });
 
+    request.messages.sort((a, b) => a.timestamp - b.timestamp);
+
+    const friendMessageContainer = document.getElementById("divFriendMessages");
+    const userMessageContainer = document.getElementById("divUserMessages");
+
+    for (let i = 0; i < request.messages.length; i++){
+        let div = document.createElement("div");
+        div.innerHTML = 'User: ' + request.messages[i].user + '' + request.messages[i].message;
+        if (request.messages[i].user === "Johan") {
+            friendMessageContainer.appendChild(div);
+        }
+        else {
+            userMessageContainer.appendChild(div);
+        }
+    }
 }
+
+addEventListener('submit', appendMessages);
+
+async function appendMessages(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const formData = new FormData(form);
+    const formInput = Object.fromEntries(formData);
+    console.log(formInput);
+    await fetch(`${baseUrl}/api/messages/append`, {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer N31fRWVMZCtwU0JeZnBQdVBjTmlOImRzcTAxfl08cz1xR2lyWGFJfmo5JC5RNSc=',
+            'Content-type': ''
+        },
+        body: {message: formInput.inputMessage}
+        //body: JSON.stringify({message: `${formInput}`})
+    });
+}
+
